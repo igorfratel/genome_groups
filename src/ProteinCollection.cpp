8d44e1754ffe34f828fc9aab204ffe3bdf0016f7
igorfratel <igorfratelsantana@gmail.com>
2018-01-03 19:57:40 -0200

    untested integration of unionfind with hashtable

--- src/ProteinCollection.cpp
+++ src/ProteinCollection.cpp
@@ -1,8 +1,12 @@
 //Union-Find implementation
 
-#include "UnionFind.h"
+#include "ProteinCollection.h"
 
-UnionFind::UnionFind(int n) {
+ProteinCollection::ProteinCollection() {}
+
+ProteinCollection::ProteinCollection(int n) { //!!! resize bigger may be better
+    protein_count = 0;
+    protein_indexes = HashTable(n);
     count = n;
     parent.resize(n);
     size.resize(n);
@@ -12,10 +16,37 @@ UnionFind::UnionFind(int n) {
     }
 }
 
-void UnionFind::connect_proteins(std::string p, std::string q) {
+void ProteinCollection::add_protein(std::string p){
+        if(get_number_proteins() == parent.size()){
+            parent.resize(parent.size() + 1);
+            size.resize(parent.size() + 1);
+            parent[parent.size() - 1] = parent.size() - 1;
+            size[size.size() - 1] = size.size() - 1;
+        }
+        protein_indexes.insert(p, get_number_proteins());
+        protein_count++;
+}
+
+void ProteinCollection::add_connected_proteins(std::string p, std::string q){
+    add_protein(p);
+    add_protein(q);
+    connect_proteins(p, q);
+}
+
+
+double ProteinCollection::get_similarity(std::string p, std::string q){
+    if(are_connected(p, q)) return 1.0;
+    return 0.0;
+}
+
+int ProteinCollection::get_number_proteins(){
+    return protein_count;
+}
+
+void ProteinCollection::connect_proteins(std::string p, std::string q) {
     int rootP = find(p);
     int rootQ = find(q);
-    if (rootP == rootQ) return;
+    if (rootP == rootQ || rootP == -1 || rootQ == -1) return;
 
     // make smaller root point to larger one
     if (size[rootP] < size[rootQ]) {
@@ -28,8 +59,9 @@ void UnionFind::connect_proteins(std::string p, std::string q) {
     }
     count--;
 }
-int UnionFind::find(int p) {
-    int root = p;
+int ProteinCollection::find(std::string p) {
+    int root = protein_indexes.get_value(p);
+    if (root == -1) return -1;
     while (root != parent[root])
         root = parent[root];
     while (p != root) {
@@ -39,11 +71,11 @@ int UnionFind::find(int p) {
     }
     return root;
 }
-bool UnionFind::connected(int p, int q){
+bool ProteinCollection::are_connected(std::string p, std::string q){
     return find(p) == find(q);
 }
 
-int UnionFind::get_count(){
+int ProteinCollection::get_count(){
     return count;
 
 }
--- src/ProteinCollection.h
+++ src/ProteinCollection.h
@@ -2,43 +2,37 @@
 #define __PROTEIN_COLLECTION_H__
 
 #include <vector>
+#include "HashTable.h"
 
 //Union-Find implementation
 class ProteinCollection{
+    HashTable protein_indexes;
     std::vector<int> parent; // parent[i] = parent of i
     std::vector<int> size;   // size[i] = number of sites in tree rooted at i
                              // Note: not necessarily correct if i is not a root node
     int count; // number of components
 
     public:
-        void add_protein();
-
-        void Union(int p, int q);
-        int find(int p);
-        bool connected(int p, int q);
-        int get_count();
-
-
         ProteinCollection();
 
 		/*Creates object with known number of proteins to be added*/
 		ProteinCollection(int n);
 
 		/*Adds protein with string identifier*/
-		void add_protein(std::string node);
+		void add_protein(std::string p);
 
 		/*Adds connection between two existing proteins*/
-		void connect_proteins(std::string node1, std::string node2);
+		void connect_proteins(std::string p, std::string q);
 
-		/*Adds two proteins and connects them with given similarity*/
-		void add_connected_proteins(std::string node1, std::string node2, double weight);
+		/*Adds two proteins and connects them.*/
+		void add_connected_proteins(std::string p, std::string q);
 
 		/*Returns true if given proteins are directly connected and false otherwise*/
-		bool are_connected(std::string node1, std::string node2);
+		bool are_connected(std::string p, std::string q);
 
 		/*Returns similarity value between two existing and directly connected proteins.
 		 *If not connected, returns 0.0*/
-		double get_similarity(std::string node1, std::string node2);
+		double get_similarity(std::string p, std::string q);
 
 		/*Returns number of proteins in object*/
 		int get_number_proteins();
@@ -48,6 +42,9 @@ class ProteinCollection{
 		std::vector<std::vector<std::string> > connected_components(double weight);
 
     private:
+        int protein_count;
+        int find(std::string p);
+        int get_count();
 
 
 };
